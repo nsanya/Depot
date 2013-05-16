ca65c661b8335254af1e81fed7166be79b9dbf65
nsanya <nsanya@inbox.ru>
2013-05-16 15:33:56 +0300

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: {minimum: 3, maximum: 15}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)$}i,
     message: 'adress is not correct'
