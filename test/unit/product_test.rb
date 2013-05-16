e222b13a003a4db2aba9dbdaf6fb66239a9dfcc6
nsanya <nsanya@inbox.ru>
2013-05-16 15:19:58 +0300

    Validation!

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,76 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+#
+#
+  test "product attributes must not be empty" do
+    #свойства товара не должны быть пустыми
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  
+  test "product price must be positive" do
+    #цена товара должна быть положительной
+    product = Product.new(title: "my title", description: "my description",
+                          image_url: "test.jpg"
+                          #:description => "yyy",тот же самый хеш, просто другой вид записи
+                          )
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join("; ")
+    
+    product.price = -0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join("; ")
+    
+    product.price = 1
+    assert product.valid?
+    
+  end
+
+def new_product(image_url)
+    Product.new(title:       "My Book Title",
+                description: "yyy",
+                price:       1,
+                image_url:   image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy", 
+                          price:       1, 
+                          image_url:   "fred.gif")
+
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+  
+  test "product is not valid without a unique title" do
+    #если у товара нет уникального названия, то он недопустим
+    product = Product.new(title: 'Programming Ruby 1.9', description: "yyy",
+                          price: 1, image_url: "img.gif")
+    assert !product.save
+    #assert_equal "has alredy been taken", product.errors[:title].join(', ')
+    assert_equal I18n.translate('activerecords.errors.messages.taken'), product.errors[:title].join(', ')
+      
+  end
+
 end
